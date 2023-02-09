99c49def58093f646743627783ad277235d412ae
Boris Manojlovic <boris@steki.net>
2023-02-09 10:41:11 +0100

    add hostname it works on

--- helloworld.go
+++ helloworld.go
@@ -8,7 +8,11 @@ import (
 
 func main() {
 	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		hostname, err = os.Hostname()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Fprintf(w, "Hello, World on %s!", hostname)
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
